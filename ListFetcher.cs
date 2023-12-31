5a6723a0d4dbf95e6cb2f169389a534ad75420d4
Nuno Teixeira <nunowallenstein@gmail.com>
2023-12-31 18:21:02 +0000

    asyncenumerable implementation added

---
+++ ListFetcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncEnumerablesExample
+{
+    internal class ListFetcher
+    {
+        private readonly Random _randomGen;
+
+        internal ListFetcher()
+        {
+            _randomGen = new Random();
+        }
+
+        public async Task<IEnumerable<int>> GetAllInts()
+        {
+            var list = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(1000);
+                list.Add(_randomGen.Next(1, 100));
+            }
+            return list;
+        }
+
+        public async IAsyncEnumerable<int> GetAllIntsAsyncEnumerable()
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(1000);
+                yield return _randomGen.Next(1, 100);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AsyncEnumerablesExample;
+
+static class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Press a key to start the extraction number by number");
+        Console.ReadKey();
+
+        var listFetcher = new ListFetcher();
+
+        await foreach (var item in listFetcher.GetAllIntsAsyncEnumerable())
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("Press a key to start the extraction numbers by batch");
+        Console.ReadKey();
+
+        //this will fetch all data first and then it will print the result on the screen on 1 go
+
+        foreach (var item in await listFetcher.GetAllInts())
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
